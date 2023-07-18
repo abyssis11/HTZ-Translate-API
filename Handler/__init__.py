fa45c1a9e1888273afbf52d9e9082e9e0fa0c17d
abyssis11 <denikernjus@gmail.com>
2023-07-18 23:39:53 +0200

    testing 3

--- Handler/__init__.py
+++ Handler/__init__.py
@@ -2,6 +2,8 @@
 import logging
 import azure.functions as func
 from flask import Flask, request, jsonify
+from ..TranslateFunctions.parsing import translate as pars_translate
+from ..TranslateFunctions.prompting import translate as prompt_translate
 #from FlaskApp import app
 
 app = Flask(__name__)
@@ -24,3 +26,21 @@ def index():
 def hello(name: str):
     logging.info("Python HTTP trigger function processed a request")
     return f"hello {name}"
+
+@app.route("/prompting-translate", methods = ["POST"])
+def get_request_data_prompting():
+    request_data = request.get_json()
+    response = prompt_translate(request_data)
+
+    return jsonify(response), 200
+
+@app.route("/parsing-translate", methods = ["POST"])
+def get_request_data_parsing():
+    request_data = request.get_json()
+    
+    edit_api_request = request_data["EditAPI"]
+    traslate_api_request = request_data["TranslateAPI"]
+    response = pars_translate(edit_api_request, traslate_api_request)
+
+    return jsonify(response), 200
+ 
